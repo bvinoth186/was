87f37b85a69e83bc43749491d13f36cd4ba4ef20
Vinoth <vinoth186@gmail.com>
2019-03-10 17:29:51 +0530

    TLSv1.2 scripting

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,9 @@ COPY app.ear /work/config/app.ear
 COPY SSMAdmin2.ear /work/config/SSMAdmin2.ear
 COPY SSMServer2.ear /work/config/SSMServer2.ear
 COPY install_app.py /work/config/install_app.py
-COPY postgresql-42.2.5.jar /work/config/postgresql-42.2.5.jar
+COPY postgresql-9.4-1206-jdbc42.jar /work/config/postgresql-9.4-1206-jdbc42.jar
 COPY ibmcloud.cer /work/config/ibmcloud.cer
 COPY was-config.props /work/config/was-config.props
+COPY --chown=was:was ssl.client.props /opt/IBM/WebSphere/AppServer/profiles/AppSrv01/properties/ssl.client.props
 RUN /work/configure.sh
 EXPOSE 9443 9043 
--- install_app.py
+++ install_app.py
@@ -69,6 +69,33 @@ def getName (objectId):
         stIndex = 1
     return objectId[stIndex:endIndex+1]
 
+print "Enabling TLS1.2 ..."
+AdminTask.listCertStatusForSecurityStandard('[-fipsLevel SP800-131]')
+AdminTask.convertCertForSecurityStandard('[-fipsLevel SP800-131]')
+AdminTask.enableFips('[-enableFips true -fipsLevel SP800-131]')
+print AdminTask.getFipsInfo()
+AdminConfig.save()	
+
+print "Installing Postgresql TLS1.2 Certificate..."
+
+AdminTask.addSignerCertificate('[-keyStoreName NodeDefaultTrustStore -keyStoreScope (cell):DefaultCell01:(node):DefaultNode01 -certificateAlias postgres -certificateFilePath ${CUST_PATH}/ibmcloud.cer -base64Encoded true]')
+AdminConfig.save()
+
+print "Installing J2C Auth data..."
+
+security = AdminConfig.getid('/Cell:DefaultCell01/Security:/')
+alias = ['alias', 'DefaultNode01/postgreAuth']
+userid = ['userId', 'ibm_cloud_9b4b8a88_9f9b_4687_a36d_9099038efbae']
+password = ['password', 'dae62724fbf067a147103b334b361e6323e006aa75f3647efaed1660314d5ae6']
+jaasAttrs = [alias, userid, password]
+AdminConfig.create('JAASAuthData', security, jaasAttrs)
+AdminConfig.save()
+
+print "Installing JDBC provider..."
+
+AdminJDBC.createJDBCProvider("DefaultNode01", "server1", "PostgreSQL JDBC Provider", "org.postgresql.jdbc2.optional.ConnectionPool", "classpath=${CUST_PATH}/postgresql-9.4-1206-jdbc42.jar, description='PostgreSQL JDBC Provider', providerType='PostgreSQL JDBC Driver Provider'") 
+AdminConfig.save()
+
 print "Installing application ..."
 
 node = getName(getNodeId(""))
@@ -96,11 +123,13 @@ parms2 += " -nouseMetaDataFromBinary"
 AdminApp.install("/work/config/SSMServer2.ear", [parms2])
 
 print "Creating Custom Path Env Variable ..."
+
 AdminTask.setVariable('[-variableName CUST_PATH -variableValue /work/config/ -scope Cell=DefaultCell01]')
 AdminTask.showVariables ('[ -scope Cell=DefaultCell01 -variableName CUST_PATH ]')
 AdminConfig.save()
 
 print "Installing HostAlias ..."
+
 AdminConfig.create('HostAlias', AdminConfig.getid('/VirtualHost:default_host/'), '[[port "31230"] [hostname "*"]]')
 AdminConfig.create('HostAlias', AdminConfig.getid('/VirtualHost:admin_host/'), '[[port "31240"] [hostname "*"]]')
 AdminConfig.save()
