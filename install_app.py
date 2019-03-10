78d33c2c39a486b57b334858fe5842d18da3b9ad
Vinoth <vinoth186@gmail.com>
2019-03-10 20:26:16 +0530

    Update install_app.py

--- install_app.py
+++ install_app.py
@@ -114,6 +114,23 @@ parms2 += " -node " + node + " -server " + server
 parms2 += " -nouseMetaDataFromBinary"
 AdminApp.install("/work/config/SSMServer2.ear", [parms2])
 
+print "Installing JDBC provider..."
+
+AdminJDBC.createJDBCProvider("DefaultNode01", "server1", "PostgreSQL JDBC Provider", "org.postgresql.jdbc2.optional.ConnectionPool", "classpath=${CUST_PATH}/postgresql-9.4-1206-jdbc42.jar, description='PostgreSQL JDBC Provider', providerType='PostgreSQL JDBC Driver Provider'") 
+AdminConfig.save()
+
+print "Installing J2C Auth data..."
+
+security = AdminConfig.getid('/Cell:DefaultCell01/Security:/')
+print security
+alias = ['alias', 'DefaultNode01/postgreAuth']
+userid = ['userId', 'ibm_cloud_9b4b8a88_9f9b_4687_a36d_9099038efbae']
+password = ['password', 'dae62724fbf067a147103b334b361e6323e006aa75f3647efaed1660314d5ae6']
+jaasAttrs = [alias, userid, password]
+print jaasAttrs
+AdminConfig.create('JAASAuthData', security, jaasAttrs)
+AdminConfig.save()
+
 print "Enabling TLS1.2 ..."
 AdminTask.listCertStatusForSecurityStandard('[-fipsLevel SP800-131]')
 AdminTask.convertCertForSecurityStandard('[-fipsLevel SP800-131]')
